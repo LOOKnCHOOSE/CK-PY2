5db65d49d96c0289d1a89ee5d5ec80a2e36cf2fd
LOOKnCHOOSE <118387033+LOOKnCHOOSE@users.noreply.github.com>
2022-12-10 16:38:39 +0300

    Лабораторная работа 5

---
+++ Лабораторная работа 5/task_1.py
@@ -0,0 +1,9 @@
+from pprint import pprint # загружаем модуль pprint
+slovar = []
+n = 16 # Для получения списка от до 15
+
+for a in range(n):
+    b = {'bin': bin(a), 'dec': a, 'hex': hex(a), 'oct': oct(a)} # Создание словаря от текущего значения
+    slovar.append(b)
+
+pprint(slovar)
